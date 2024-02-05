068573771fc4bff0c404b33aa5e07584a51601eb
Yuyang Huang <yuyanghuang@google.com>
2024-02-06 05:19:07 +0900

    v5: rename JNEBS_OPCODE to JBSMATCH_OPCODE for consistency
    Test: TH
    Change-Id: If0d37ede2d3ad1c51eb7553b794fe2dcb2acd825

--- v5/apf_interpreter.c
+++ v5/apf_interpreter.c
@@ -233,7 +233,7 @@ typedef union {
 #define JGT_OPCODE 17   /* Compare greater than and branch, e.g. "jgt R0,5,label" */
 #define JLT_OPCODE 18   /* Compare less than and branch, e.g. "jlt R0,5,label" */
 #define JSET_OPCODE 19  /* Compare any bits set and branch, e.g. "jset R0,5,label" */
-#define JNEBS_OPCODE 20 /* Compare not equal byte sequence, e.g. "jnebs R0,5,label,0x1122334455" */
+#define JBSMATCH_OPCODE 20 /* Compare byte sequence [R=0 not] equal, e.g. "jbsne R0,2,label,0x1122" */
 #define EXT_OPCODE 21   /* Immediate value is one of *_EXT_OPCODE */
 #define LDDW_OPCODE 22  /* Load 4 bytes from data address (register + simm): "lddw R0, [5+R1]" */
 #define STDW_OPCODE 23  /* Store 4 bytes to data address (register + simm): "stdw R0, [5+R1]" */
@@ -720,7 +720,7 @@ int apf_run(void* ctx, u8* const program, const u32 program_len,
           case JGT_OPCODE:
           case JLT_OPCODE:
           case JSET_OPCODE:
-          case JNEBS_OPCODE: {
+          case JBSMATCH_OPCODE: {
               /* Load second immediate field. */
               u32 cmp_imm = 0;
               if (reg_num == 1) {
@@ -736,7 +736,7 @@ int apf_run(void* ctx, u8* const program, const u32 program_len,
                   case JGT_OPCODE:  if (registers[0] >  cmp_imm) pc += imm; break;
                   case JLT_OPCODE:  if (registers[0] <  cmp_imm) pc += imm; break;
                   case JSET_OPCODE: if (registers[0] &  cmp_imm) pc += imm; break;
-                  case JNEBS_OPCODE: {
+                  case JBSMATCH_OPCODE: {
                       /* cmp_imm is size in bytes of data to compare. */
                       /* pc is offset of program bytes to compare. */
                       /* imm is jump target offset. */
